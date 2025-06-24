c893c7b65195c16d71eec9e2b63f3a888e2736c0
Benjamin Bryant <100496281+Bencb03@users.noreply.github.com>
2025-06-24 12:40:47 -0400

    Personal Website started with Angular framework

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home';
+import { ProjectsComponent } from './projects/projects';
+import { ContactComponent } from './contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'contact', component: ContactComponent },
+];
+
+
